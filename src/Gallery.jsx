cbf41062ca74eb8638665f35eec4852df86f3bce
Leonardo Lopez <96030802+lejolo20@users.noreply.github.com>
2024-08-21 19:28:59 -0400

    feat: adding dots and animation

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { useState } from "react";
 import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
+import { FaRegDotCircle, FaDotCircle } from "react-icons/fa";
 
 const Gallery = () => {
   const [imageIndex, setImageIndex] = useState(0);
 
   const Image = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
-
-  const image = Image.map((item) => {
-    return <img className="displayed-image" src={item} alt="" />;
-  });
 
   const prevImg = () => {
     imageIndex === 0
@@ -23,15 +20,46 @@ const Gallery = () => {
       : setImageIndex(imageIndex + 1);
   };
   return (
-    <div className="image-container">
-      {image[imageIndex]}
-      <button onClick={prevImg} className="prevBtn">
-        <AiFillCaretLeft style={{ color: "white" }} />
-      </button>
-      <button onClick={nextImg} className="nextBtn">
-        <AiFillCaretRight style={{ color: "white" }} />
-      </button>
-    </div>
+    <>
+      <div className="image-container">
+        <div className="image-slider">
+          {Image.map((item, index) => {
+            return (
+              <img
+                key={index}
+                className="displayed-image"
+                style={{ translate: `${-100 * imageIndex}%` }}
+                src={item}
+                alt=""
+              />
+            );
+          })}
+        </div>
+        <button onClick={prevImg} className="prevBtn">
+          <AiFillCaretLeft style={{ color: "white" }} />
+        </button>
+        <button onClick={nextImg} className="nextBtn">
+          <AiFillCaretRight style={{ color: "white" }} />
+        </button>
+        <div className="indexBtn-container">
+          {Image.map((item, index) => {
+            return (
+              <button
+                key={index}
+                className="indexBtn"
+                onClick={() => setImageIndex(index)}
+              >
+                {index === imageIndex ? (
+                  <FaDotCircle className="circle1" />
+                ) : (
+                  <FaRegDotCircle className="circle2" />
+                )}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 };
 
